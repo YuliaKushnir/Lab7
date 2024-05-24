b246615fd88bdbc963f6df15b2ea498905c1b706
LavY <148194874+YuliaKushnir@users.noreply.github.com>
2024-05-24 09:39:11 +0300

    modified:   script/task8.js

--- script/task7.js
+++ script/task7.js
@@ -1,13 +1,10 @@
-// Отримуємо елемент ul#categories
 const categoriesList = document.getElementById("categories");
 
 const categoryItems = categoriesList.querySelectorAll("li.item");
 
-// Порахуємо і виведемо в консоль кількість категорій
 const categoryAmount = categoryItems.length;
 console.log(`Кількість категорій: ${categoryAmount}`);
 
-// Виведемо текст заголовку та кількість елементів в категорії
 categoryItems.forEach((categoryItem) => {
   const categoryTitle = categoryItem.querySelector("h2").textContent;
 
--- script/task8.js
+++ script/task8.js
@@ -1,33 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Отримуємо колекцію елементів за їх класом "login-form" і беремо перший елемент з колекції
     const loginForm = document.querySelector(".login-form");
 
-    // Додаємо обробник події "submit" для форми
     loginForm.addEventListener("submit", function (event) {
-      // Запобігаємо стандартній дії браузера при відправці форми (перезавантаженні сторінки)
       event.preventDefault();
 
-      // Отримуємо значення полів вводу
       const emailInput = loginForm.elements.email;
       const passwordInput = loginForm.elements.password;
 
-      // Перевіряємо, чи всі поля заповнені (trim() для видалення зайвих пробілів)
       if (!emailInput.value.trim() || !passwordInput.value.trim()) {
-        // Виводимо повідомлення, якщо не всі поля заповнені
         alert("All form fields must be filled in");
-        return; // Завершуємо функцію, щоб не продовжувати обробку
+        return; 
       }
 
-      // Збираємо значення полів в об'єкт
       const formData = {
         email: emailInput.value.trim(),
         password: passwordInput.value.trim(),
       };
 
-      // Виводимо об'єкт з введеними даними в консоль
       console.log(formData);
 
-      // Очищаємо значення полів форми
       loginForm.reset();
     });
 });
